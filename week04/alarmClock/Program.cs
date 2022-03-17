449e13d4b10b03b44877d3dd2d076bec0be4ad3d
LinDongxx <99698998+LinDongxx@users.noreply.github.com>
2022-03-17 23:52:34 +0800

    week04

---
+++ week04/alarmClock/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace AlarmEvent
+{
+    public class AlarmClock
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock clock = new AlarmClock();
+            clock.Tick += new TickEventHandler((sender, e) => Console.WriteLine("处理Tick"));
+            clock.Alarm += new AlarmEventHandler((sender, e) => Console.WriteLine("处理Alarm"));
+            clock.OnTick();
+            clock.OnAlarm();
+        }
+
+        public delegate void TickEventHandler(object sender, EventArgs e);
+        public event TickEventHandler Tick;
+
+        public delegate void AlarmEventHandler(object sender, EventArgs e);
+        public event AlarmEventHandler Alarm;
+
+        public void OnTick()
+        {
+            Console.WriteLine("闹钟Tick");
+            Tick.Invoke(this, new EventArgs());
+        }
+
+        public void OnAlarm()
+        {
+            Console.WriteLine("闹钟Alarm");
+            Alarm.Invoke(this, new EventArgs());
+        }
+
+
+    }
+}
---
+++ week04/week04/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace week04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            Console.WriteLine("打印元素：");
+            intlist.ForEach(i => Console.WriteLine(i));
+            Console.WriteLine("求最大值:");
+            int max = 0;
+            intlist.ForEach(delegate (int i) { if (max < i) max = i; });
+            Console.WriteLine(max);
+            Console.WriteLine("求和:");
+            int sum = 0;
+            intlist.ForEach(i => sum += i);
+            Console.WriteLine(sum);
+            Console.WriteLine("求最小值");
+            int min = 0;
+            intlist.ForEach(delegate(int i) { if (min > i) min = i; });
+            Console.WriteLine(min);
+        }
+
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public GenericList() { head = null; tail = null; }
+            public Node<T> Head { get { return head; } }
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null) { head = n; tail = n; }
+                else { tail.Next = n; tail = n; }
+            }
+            public void ForEach(Action<T> action)
+            {
+                Node<T> temp = head;
+                while (temp.Next != null)
+                {
+                    action(temp.Data);
+                    temp = temp.Next;
+                }
+                action(temp.Data);
+            }
+        }
+
+    }
+}
+
+  
